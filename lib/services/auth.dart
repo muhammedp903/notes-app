4176a19ea25abc788633e1d3c99af97c1ad7aecd
Muhammed Patel <muhammedp903@gmail.com>
2021-04-03 01:51:53 +0100

    Changed greeting text in default first note.

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,21 +3,19 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'database.dart';
 
 class AuthService {
-
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Stream<FirebaseUser> get user {
     return _auth.onAuthStateChanged;
   }
 
-  Future signIn(String email,String password) async {
-    try{
-      AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+  Future signIn(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
       FirebaseUser user = result.user;
       return user;
-    }catch(e){
-
-    }
+    } catch (e) {}
   }
 
   Future register(String email, String password) async {
@@ -25,7 +23,9 @@ class AuthService {
       AuthResult result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       FirebaseUser user = result.user;
-      await Database(uid: user.uid).newNote("My first note", "Welcome to the notes app");
+      await Database(uid: user.uid).newNote("Welcome to the notes app.", """
+Tap on a note to see its full contents, edit any part of it, or delete it.
+You can also edit a note by tapping the edit button on its tile.""");
       return user;
     } catch (e) {
       print("Error registering");
@@ -40,5 +40,4 @@ class AuthService {
       print("Error signing out");
     }
   }
-
 }
